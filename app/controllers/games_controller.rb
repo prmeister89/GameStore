43e4ffa088b423f350e5a1b467138b54f2870c31
github username <github email address>
2018-08-20 13:38:01 -0400

    initial controller setup

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,17 @@
 class GamesController < ApplicationController
+
+  def index
+    @games = game.all
+  end
+
+  def show
+    get_game
+  end
+
+  private
+
+  def get_game
+    @game = Game.find(params[:id])
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    get_usr
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(usr_params)
+    redirect_to user_path(@user)
+  end
+
+  def edit
+    get_usr
+  end
+
+  def update
+    @user = get_usr.update(usr_params)
+    redirect_to user_path(@user)
+  end
+
+  def delete
+    redirect_to users_path
+  end
+
+  private
+
+    def get_usr
+      @user = User.find(params[:id])
+    end
+
+    def usr_params
+      params.require(:user).permit(:name, :balance)
+    end
+
 end
